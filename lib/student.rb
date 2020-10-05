d6a7927a4c5b898e7015cca14098569df9f4c882
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-05 18:42:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,8 +25,18 @@ class Student
   def self.drop_table
     sql =  <<-SQL
       DROP TABLE students
-      SQL
+    SQL
     DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+
   end
 
 end
