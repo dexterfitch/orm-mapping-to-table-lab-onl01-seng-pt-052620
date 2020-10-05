0e51f35de1f33e6d6418b21f440f0635c48256da
Dexter Fitch <fitch.dexter@gmail.com>
2020-10-05 18:33:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,12 @@ class Student
   attr_accessor :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  @@all = []
+  
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+    @@all << self
+  end
 
 end
